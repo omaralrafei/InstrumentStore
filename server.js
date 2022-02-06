f742553fa6528e09875cccb31f4b6a7c0181c4a2
omar al rafei <omar.alrafei@gmail.com>
2022-02-06 03:28:03 +0200

    l

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "public/views/"));
 app.use(express.urlencoded({ extended: true }));
 
-
+//sdgadfg
 
 //connects to DB and creates collection Cart
 MongoClient.connect(connectionURL, function (error, database) {
